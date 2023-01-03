d3c3ac7723a69511b660340b4d2f6b34c3d0e72c
ssantiago90 <salkin.santiago@gmail.com>
2023-01-03 11:43:45 -0300

    search function

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ export default function App() {
   const [dataGifs, setDataGifs] = useState([]);
 
   const APIKEY = "Icvi503hE2SlujmS9Q9r8bvlgX5Zgue9";
-  const searchwords = "cats";
 
-  const url = `http://api.giphy.com/v1/gifs/search?q=${searchwords}&api_key=${APIKEY}&limit=10`;
+  function fetchGifs(searchwords) {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${searchwords}&api_key=${APIKEY}&limit=10`;
 
-  function fetchGifs() {
     fetch(url)
       .then((respuesta) => respuesta.json())
       .then((json) => setDataGifs(json.data));
   }
 
+  function handleInputSearch(evt) {
+    const searchword = evt.target.value;
+    fetchGifs(searchword);
+  }
+
   return (
     <div>
       <h1>CODERGIFS</h1>
-      <button onClick={fetchGifs}>Get gif</button>
+
+      <label>
+        Buscar:
+        <input onChange={handleInputSearch} name="searchword"></input>
+      </label>
+
       {dataGifs.map((item) => (
         <GifImg
           key={item.id}
