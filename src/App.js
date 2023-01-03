c7c0e8f6a465e2378751e770ae7f23ecb7222d2d
ssantiago90 <salkin.santiago@gmail.com>
2023-01-02 22:55:02 -0300

    34855

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import React, { useState } from "react";
 export default function App() {
   const [dataGifs, setDataGifs] = useState([]);
 
-  const APIKEY = "EKhf4RvvCY28KgzIN1lnGlSNkOMO27dH";
+  const APIKEY = "Icvi503hE2SlujmS9Q9r8bvlgX5Zgue9";
+  const searchwords = "cats";
 
-  const url = `http://api.giphy.com/v1/gifs/search?q=simpsons&api_key=EKhf4RvvCY28KgzIN1lnGlSNkOMO27dH`;
+  const url = `http://api.giphy.com/v1/gifs/search?q=${searchwords}&api_key=${APIKEY}&limit=10`;
 
   function fetchGifs() {
-    console.log("fetch...");
+    fetch(url)
+      .then((respuesta) => respuesta.json())
+      .then((json) => setDataGifs(json.data));
   }
 
   return (
     <div>
       <h1>CODERGIFS</h1>
       <button onClick={fetchGifs}>Get gif</button>
+      {dataGifs.map((item) => (
+        <GifImg
+          key={item.id}
+          url={item.images.downsized_large.url}
+          title={item.title}
+        />
+      ))}
     </div>
   );
 }
