0b1bea6475ed1889a185d4205152c08f0c191da7
ssantiago90 <salkin.santiago@gmail.com>
2023-01-05 21:17:58 -0300

    clase7

--- src/App.js
+++ src/App.js
@@ -6,16 +6,28 @@ export default function App() {
 
   const APIKEY = "EKhf4RvvCY28KgzIN1lnGlSNkOMO27dH";
 
-  const url = `http://api.giphy.com/v1/gifs/search?q=simpsons&api_key=EKhf4RvvCY28KgzIN1lnGlSNkOMO27dH`;
-
-  function fetchGifs() {
-    console.log("fetch...");
+  function fetchGifs(searchKeyword) {
+    let url = `https://api.giphy.com/v1/gifs/search?q=${searchKeyword}&api_key=${APIKEY}&limit=10`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json.data);
+        setDataGifs(json.data);
+      });
   }
 
   return (
     <div>
       <h1>CODERGIFS</h1>
-      <button onClick={fetchGifs}>Get gif</button>
+      <button onClick={() => fetchGifs("pokemon")}>Get Pokemons</button>
+      <button onClick={() => fetchGifs("cats")}>Get Cats</button>
+      {dataGifs.map((gif) => (
+        <GifImg
+          key={gif.title}
+          url={gif.images.downsized.url}
+          title={gif.title}
+        />
+      ))}
     </div>
   );
 }
