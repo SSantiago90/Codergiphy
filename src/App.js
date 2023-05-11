ae3dc8127bd520fea6dc6305b6df4ebe16d556f3
ssantiago90 <salkin.santiago@gmail.com>
2023-05-11 11:47:50 -0300

    43210

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import GifImg from "./GifImg";
 import React, { useState } from "react";
+import GifImg from "./GifImg";
 
 export default function App() {
-  const [dataGifs, setDataGifs] = useState([]);
+  const [gifs, setGifs] = useState([]);
+  const [keyword, setKeyword] = useState("pokemon");
 
-  const APIKEY = "EKhf4RvvCY28KgzIN1lnGlSNkOMO27dH";
+  const api_key = "ll5jVWbjSxKR3YSbIpWFxQsFsVGIbH3k";
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${keyword}&limit=10`;
 
-  const url = `http://api.giphy.com/v1/gifs/search?q=simpsons&api_key=EKhf4RvvCY28KgzIN1lnGlSNkOMO27dH`;
+  async function fetchGifs() {
+    const reponseAPI = await fetch(url);
+    console.log(reponseAPI);
+    const jsonAPI = await reponseAPI.json();
+    console.log(jsonAPI);
+    setGifs(jsonAPI.data);
+  }
 
-  function fetchGifs() {
-    console.log("fetch...");
+  function handleChangeKeyword(evt) {
+    setKeyword(evt.target.value);
   }
 
   return (
-    <div>
+    <div
+      style={{
+        margin: "0px auto",
+        padding: "10px 50px",
+      }}
+    >
       <h1>CODERGIFS</h1>
+      <input
+        onChange={handleChangeKeyword}
+        placeholder="pokemon"
+        name="keyword"
+      ></input>
       <button onClick={fetchGifs}>Get gif</button>
+      <div
+        style={{
+          justifyContent: "space-around",
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
+        {gifs.map((gif) => (
+          <GifImg
+            key={gif.title}
+            url={gif.images.downsized_medium.url}
+            title={gif.title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/GifImg.jsx
+++ src/GifImg.jsx
@@ -1,6 +1,12 @@
 function GifImg({ url, title }) {
+  const styleGif = {
+    border: "solid 1px #87a0b0",
+    borderRadius: "10px",
+    padding: "15px",
+    marginBottom: "25px",
+  };
   return (
-    <div>
+    <div style={styleGif}>
       <h4>{title}</h4>
       <img src={url} alt={title} />
     </div>
